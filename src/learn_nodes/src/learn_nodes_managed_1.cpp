0e6f2a9038f52581969e86f23175101f85511ad0
Abin Alex Pothen <abinalexpothen@gmail.com>
2026-02-15 13:48:24 -0500

    Setup cmake to register two managed nodes as components

---
+++ src/learn_nodes/src/learn_nodes_managed_1.cpp
@@ -0,0 +1,19 @@
+/* Managed node 1 for the learn nodes exercise */
+
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_lifecycle/lifecycle_node.hpp"
+
+namespace learn_nodes_exercise 
+{
+    class LearnNodesManaged1 : public rclcpp_lifecycle::LifecycleNode
+    {
+        public:
+        LearnNodesManaged1(const rclcpp::NodeOptions &options) : rclcpp_lifecycle::LifecycleNode("learn_nodes_managed_1", options)
+        {
+            // some constructor
+        }
+    };
+}
+
+#include "rclcpp_components/register_node_macro.hpp"
+RCLCPP_COMPONENTS_REGISTER_NODE(learn_nodes_exercise::LearnNodesManaged1)
